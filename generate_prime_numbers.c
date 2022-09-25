2f553ee09d74f89fb065c287b55e5b84013caf0f
cludmus <114337269+cludmus@users.noreply.github.com>
2022-09-25 13:17:08 +0300

    Create generate_prime_numbers.c

---
+++ generate_prime_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int* get_prime_numbers(int size){
+    int cursor = 0;
+    int* primes = (int*) malloc (size*sizeof(int));
+    primes[cursor++] = 2;
+
+    for(int i = 3; cursor < size; i++){
+        char has_divisor = 0;
+        for(int j = 0; j < cursor && primes[j]*primes[j] <= i; j++){
+            if(i % primes[j] == 0){
+                has_divisor = 1;
+                break;
+            }
+        }
+        if(!has_divisor)
+            primes[cursor++] = i;
+    }
+    return primes;
+}
+
+int main(){
+    // 1 000 000 - ~2 seconds
+    // 2 000 000 - ~5 seconds
+    // 3 000 000 - ~10 seconds
+    int* primes = get_prime_numbers(1000);
+}
